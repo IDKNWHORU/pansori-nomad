10337c79d2dc35f008c013f507bda9a392ba3c16
junho ra <junho9341@gmail.com>
2023-10-29 04:00:27 +0000

    add image load

--- src/main/java/com/example/demo/nft/NFTController.java
+++ src/main/java/com/example/demo/nft/NFTController.java
@@ -1,8 +1,13 @@
 package com.example.demo.nft;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +24,22 @@ public class NFTController {
         return "hello nft and " + id;
     }
 
+    @PostMapping("/{id}")
+    public boolean createNft(@PathVariable String id,
+            @RequestParam String a) {
+        return true;
+    }
+
+    @GetMapping("/image")
+    public ResponseEntity<Resource> showImage() {
+        try {
+            Resource image = new ClassPathResource("static/images/image.jpg");
+
+                return ResponseEntity.ok()
+                    .header("Content-Type", "image/jpeg")
+                    .body(image);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
